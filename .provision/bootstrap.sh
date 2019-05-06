20ba6d7e77e595491eb12c144e183dbbe4d6ce2b
Christian Wang <christian.gnaw@gmail.com>
2019-05-06 13:07:30 -0400

    Added vagrant init

---
+++ .provision/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#setup nodejs
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#setup yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+sudo sh -c 'echo "deb https://dl.yarnpkg.com/debian/ stable main" >> /etc/apt/sources.list.d/yarn.list'
+sudo apt-get install -y yarn
+
+#setup mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+sudo apt-get install -y mongodb-org
+
+#ip binding conf
+sudo mv /home/config/mongod.conf /etc/mongod.conf
+
+#setup redis
+sudo add-apt-repository -y ppa:chris-lea/redis-server
+sudo apt-get install -y redis-server
+
+#ip binding conf
+sudo mv /home/config/redis.config /etc/redis.conf
